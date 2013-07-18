f75a6e6d944a970bfb36f5e8163d9af1ec52b2d1
nilo83link <danilo.balocco@gmail.com>
2013-07-18 22:21:29 +0200

    change port to 8080

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var fs = require('fs');
 
+var htmlfile = "index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html", 'utf8'));
+  response.send(fs.readFileSync(htmlfile, 'utf8'));
   //response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
